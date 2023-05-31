b4e73dff3cf74ee551c11d2a847b61420ca27001
Andrei Coman <comandrei@gmail.com>
2023-05-31 20:04:47 +0300

    Add initial model

--- sample_project/curs/models.py
+++ sample_project/curs/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Curs(models.Model):
+    durata = models.IntegerField()
+    pret = models.FloatField()
+    descriere = models.TextField()
+    nume = models.CharField(max_length=30)
