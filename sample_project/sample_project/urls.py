b2209bf210dadf79fc02a33c6b7068dfaa62a0b8
Andrei Coman <comandrei@gmail.com>
2023-05-31 19:06:33 +0300

    Add first custom view

--- sample_project/sample_project/urls.py
+++ sample_project/sample_project/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from curs.views import salut
+
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("salut", salut),
 ]
