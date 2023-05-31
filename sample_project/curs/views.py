f10a914b180819874fdfbbbe34b8ada99dbd24c4
Andrei Coman <comandrei@gmail.com>
2023-05-31 19:50:53 +0300

    Url capturing

--- sample_project/curs/views.py
+++ sample_project/curs/views.py
@@ -3,4 +3,8 @@ from django.http import HttpResponse
 # Create your views here.
 
 def salut(request):
+    unu = 1 
     return HttpResponse("Salut!")
+
+def salut_nume(request, nume):
+    return HttpResponse(f"Salut {nume}!")
--- sample_project/sample_project/urls.py
+++ sample_project/sample_project/urls.py
@@ -16,9 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
-from curs.views import salut
+from curs.views import salut, salut_nume
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("salut", salut),
+    path("salut/<int:nume>/", salut_nume)
 ]
