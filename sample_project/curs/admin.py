1758a2b3a9714b9fcb38a1c0e34b38af0fd414e2
Andrei Coman <comandrei@gmail.com>
2023-06-05 20:07:16 +0300

    Show profesor in admin

--- sample_project/curs/admin.py
+++ sample_project/curs/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Curs
+from .models import Curs, Profesor
 
 admin.site.register(Curs)
+admin.site.register(Profesor)
