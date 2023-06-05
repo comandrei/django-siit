8737a0dff2744b471981e1b68be186c8559066c2
Andrei Coman <comandrei@gmail.com>
2023-06-05 19:24:50 +0300

    Add unique

--- sample_project/curs/models.py
+++ sample_project/curs/models.py
@@ -5,6 +5,6 @@ class Curs(models.Model):
     durata = models.IntegerField(default=80)
     pret = models.FloatField(db_index=True)
     descriere = models.TextField(null=True, blank=True)
-    nume = models.CharField(max_length=30)
+    nume = models.CharField(max_length=30, unique=True)
     creat = models.DateTimeField(auto_now_add=True)
     actualizat = models.DateTimeField(auto_now=True)
