ae34dbdfb534f0db0a01f1e8e6b5139c9c122763
Andrei Coman <comandrei@gmail.com>
2023-06-05 18:42:04 +0300

    Add default value

--- sample_project/curs/models.py
+++ sample_project/curs/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Create your models here.
 class Curs(models.Model):
-    durata = models.IntegerField()
+    durata = models.IntegerField(default=80)
     pret = models.FloatField()
     descriere = models.TextField()
     nume = models.CharField(max_length=30)
