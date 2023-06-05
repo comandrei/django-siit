34065e379dbe7eba51075436088ce704601fe00c
Andrei Coman <comandrei@gmail.com>
2023-06-05 19:18:08 +0300

    Add Index

--- sample_project/curs/models.py
+++ sample_project/curs/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 class Curs(models.Model):
     durata = models.IntegerField(default=80)
-    pret = models.FloatField()
+    pret = models.FloatField(db_index=True)
     descriere = models.TextField(null=True, blank=True)
     nume = models.CharField(max_length=30)
     creat = models.DateTimeField(auto_now_add=True)
