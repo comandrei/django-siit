423af686c0352d8ad895816991b952bf00b120f2
Andrei Coman <comandrei@gmail.com>
2023-06-12 18:39:30 +0300

    OR orm example

--- sample_project/curs/views.py
+++ sample_project/curs/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 from .models import Curs
+from django.db.models import Q
 
 def salut(request):
     unu = 1 
@@ -14,7 +15,7 @@ def cursuri(request):
     an = request.GET.get("an")
     toate_cursurile = Curs.objects.all().order_by("-an")
     if an is not None:
-        toate_cursurile = toate_cursurile.filter(an__lte=int(an), nume__contains="Curs")
+        toate_cursurile = toate_cursurile.filter(Q(an__lte=int(an)) | Q(nume__contains="Curs"))
     nume_cursuri = [f"<a href='/curs/{curs.id}'>{curs.nume} an: {curs.an}</a>" for curs in toate_cursurile]
     print(request.GET)
 
