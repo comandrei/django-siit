4b42557cde21bf9914640cb71f9710019cf2aee1
Andrei Coman <comandrei@gmail.com>
2023-06-14 20:49:56 +0300

    Configure staticfile serving

--- sample_project/sample_project/urls.py
+++ sample_project/sample_project/urls.py
@@ -16,7 +16,8 @@ Including another URLconf
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path, include
+from django.contrib.staticfiles import views
+from django.urls import re_path, path, include
 
 from curs.views import salut, salut_nume, cursuri, curs, studenti, main, profil
 
@@ -35,3 +36,4 @@ urlpatterns = [
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+    urlpatterns.append(re_path(r"^static/(?P<path>.*)$", views.serve))
