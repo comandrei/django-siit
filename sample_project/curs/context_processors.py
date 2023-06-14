ffe0a0488b79838dd11b4c06625e1e270b844a22
Andrei Coman <comandrei@gmail.com>
2023-06-14 20:42:25 +0300

    Context Processor example

---
+++ sample_project/curs/context_processors.py
@@ -0,0 +1,5 @@
+import time
+
+def siit_context(request):
+    #time.sleep(2)
+    return {"unu":1, "doi":2}
--- sample_project/sample_project/settings.py
+++ sample_project/sample_project/settings.py
@@ -67,6 +67,7 @@ TEMPLATES = [
                 "django.template.context_processors.request",
                 "django.contrib.auth.context_processors.auth",
                 "django.contrib.messages.context_processors.messages",
+                "curs.context_processors.siit_context",
             ],
         },
     },
