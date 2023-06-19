d7e5435afb92ffd4fca491b562cd4d8b2112db49
Andrei Coman <comandrei@gmail.com>
2023-06-19 20:04:30 +0300

    Form validation

--- sample_project/curs/forms.py
+++ sample_project/curs/forms.py
@@ -11,3 +11,10 @@ class ContactForm(forms.Form):
         if nume != nume.title():
             raise ValidationError("Trebuie sa incepea cu litera mare")
         return nume
+
+    def clean(self):
+        nume = self.cleaned_data["nume"]
+        email = self.cleaned_data["email"]
+        if nume not in email:
+            raise ValidationError("Numele trebuie sa apara in email")
+        return self.cleaned_data
