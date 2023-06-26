f9e3e2728cd7b61b78d33ee836bf82bf2aec49af
Andrei Coman <comandrei@gmail.com>
2023-06-26 21:05:29 +0300

    Configure admin

--- sample_project/curs/admin.py
+++ sample_project/curs/admin.py
@@ -3,6 +3,12 @@ from django.contrib import admin
 # Register your models here.
 from .models import Curs, Profesor, Student
 
-admin.site.register(Curs)
+class CursAdmin(admin.ModelAdmin):
+    list_display = ("nume", "an", "profesor", "activ")
+    list_select_related = ("profesor", )
+    list_filter = ("profesor", "activ", "an")
+    search_fields = ("nume", "profesor__nume")
+
+admin.site.register(Curs, CursAdmin)
 admin.site.register(Profesor)
 admin.site.register(Student)
