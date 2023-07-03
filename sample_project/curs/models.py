f95e5ad7caeaac465c0f2003bf2c10dbe88da4e6
Andrei Coman <comandrei@gmail.com>
2023-07-03 21:00:43 +0300

    Add last update and created

--- sample_project/curs/models.py
+++ sample_project/curs/models.py
@@ -78,3 +78,5 @@ class Todo(models.Model):
     title = models.CharField(max_length=100)
     description = models.TextField()
     completed = models.BooleanField(default=False)
+    created = models.DateTimeField(auto_now_add=True)
+    last_update = models.DateTimeField(auto_now=True)
